2655123751bf1421b0ba2b27e5796b6cf42e039c
Luis <luisbarboz17@gmail.com>
2021-10-18 21:44:26 -0500

    chat

---
+++ fire.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyD4uYByMJYGcIp705LGSM0T6TWOoZ4I5Jg",
+  authDomain: "bubblechat-43994.firebaseapp.com",
+  projectId: "bubblechat-43994",
+  storageBucket: "bubblechat-43994.appspot.com",
+  messagingSenderId: "196537405344",
+  appId: "1:196537405344:web:12acd8eff636e27db89ec5",
+  measurementId: "G-DZQ3ZC6M70"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
